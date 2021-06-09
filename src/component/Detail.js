6c62c30479f66bf4b9e500bbc2bfcbfce22831c8
Yehong Piao <yehongpiao@Yehongs-MacBook-Air.local>
2021-06-08 23:27:18 -0500

    hook4

--- src/component/Detail.js
+++ src/component/Detail.js
@@ -4,66 +4,66 @@ import moment from 'moment'
 import ReactEcharts from 'echarts-for-react';
 import {API_KEY} from '../constants'
 
-class Detail extends React.Component {
-  state={
-    renderStock: []
-  }
+function Detail (){
+  // state={
+  //   renderStock: []
+  // }
 
-  componentDidMount(){
-    const ticker = this.props.match.params.ticker
-    console.log({ticker})
-      fetch(`https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v3/get-historical-data?region=US&symbol=${ticker}`, {
-          "method": "GET",
-          "headers": {
-              "x-rapidapi-host": "apidojo-yahoo-finance-v1.p.rapidapi.com",
-              "x-rapidapi-key": `${API_KEY}`
-          }
-      })
-      .then(response => response.json())
-      .then(data => {
-          console.log(data)
-          this.setState({
-            renderStock: data.prices.reverse()
-          })
-      }).catch(err => {
-        this.setState({
-          renderStock: []
-        })
-      })
-  }
+  // componentDidMount(){
+  //   const ticker = this.props.match.params.ticker
+  //   console.log({ticker})
+  //     fetch(`https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v3/get-historical-data?region=US&symbol=${ticker}`, {
+  //         "method": "GET",
+  //         "headers": {
+  //             "x-rapidapi-host": "apidojo-yahoo-finance-v1.p.rapidapi.com",
+  //             "x-rapidapi-key": `${API_KEY}`
+  //         }
+  //     })
+  //     .then(response => response.json())
+  //     .then(data => {
+  //         console.log(data)
+  //         this.setState({
+  //           renderStock: data.prices.reverse()
+  //         })
+  //     }).catch(err => {
+  //       this.setState({
+  //         renderStock: []
+  //       })
+  //     })
+  // }
 
-  render() {
-    console.log(this.state.renderStock);
-    const option = {
-      xAxis: {
-        type: "category",
-        boundaryGap: false,
-        data: [],
-      },
-      yAxis: {
-        type: "value",
-      },
-      series: [{
-        data: [],
-        type: 'line',
-        areaStyle: {}
-      }]
-    };
+  // render() {
+  //   console.log(this.state.renderStock);
+  //   const option = {
+  //     xAxis: {
+  //       type: "category",
+  //       boundaryGap: false,
+  //       data: [],
+  //     },
+  //     yAxis: {
+  //       type: "value",
+  //     },
+  //     series: [{
+  //       data: [],
+  //       type: 'line',
+  //       areaStyle: {}
+  //     }]
+  //   };
 
-    const xAxisData = [];
-    const seriesData = [];
-    this.state.renderStock.forEach((s) => {
-      xAxisData.push(moment.unix(s.date).format("MM-DD-YYYY"));
-      seriesData.push(s.close);
-    });
+  //   const xAxisData = [];
+  //   const seriesData = [];
+  //   this.state.renderStock.forEach((s) => {
+  //     xAxisData.push(moment.unix(s.date).format("MM-DD-YYYY"));
+  //     seriesData.push(s.close);
+  //   });
 
-    option.xAxis.data = xAxisData;
-    option.series = [
-      {
-        data: seriesData,
-        type: "line",
-      },
-    ];
+  //   option.xAxis.data = xAxisData;
+  //   option.series = [
+  //     {
+  //       data: seriesData,
+  //       type: "line",
+  //     },
+  //   ];
 
     return (
       <div>
@@ -73,8 +73,6 @@ class Detail extends React.Component {
         />
       </div>
       );
-    }
-  
 }
 
 export default withRouter(Detail);
